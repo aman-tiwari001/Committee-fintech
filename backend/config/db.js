950aaa8cc891b1bf118e16263fe99bda8a50a987
GM-11 <mathuyrgopal@gmail.com>
2024-04-01 21:10:14 +0530

    added razorpay and a testing component

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,14 @@
-const mongoose = require('mongoose');
+const cors = require("cors"); // Import cors
+
+const mongoose = require("mongoose");
 const connectDB = () => {
   mongoose
-    .connect(`${process.env.MONGODB_URL}/${process.env.DB}`, {
+    .connect(`mongodb+srv://GM-11:nice1234@cluster0.wohyphv.mongodb.net/`, {
+      // .connect(`${process.env.MONGODB_URL}/${process.env.DB}`, {
       useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
-    .then(() => console.log('🔥 Connected to MongoDB'))
-    .catch((err) => console.error('💩 Failed to connect to MongoDB', err));
+    .then(() => console.log("🔥 Connected to MongoDB"))
+    .catch((err) => console.error("💩 Failed to connect to MongoDB", err));
 };
 module.exports = connectDB;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import CommitteeDashboard from './pages/CommitteeDashboard';
-import NavBar from './components/NavBar';
-import LandingPage from './pages/LandingPage';
-import Footer from './components/Footer';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CommitteeDashboard from "./pages/CommitteeDashboard";
+import NavBar from "./components/NavBar";
+import LandingPage from "./pages/LandingPage";
+import Footer from "./components/Footer";
+import TestingRazorpay from "./pages/TestingRazorpay";
 
 function App() {
   return (
@@ -11,8 +12,9 @@ function App() {
       <BrowserRouter>
         <NavBar />
         <Routes>
-          <Route path='/dashboard' element={<CommitteeDashboard />} />
-          <Route path='/' element={<LandingPage />} />
+          <Route path="/Dashboard" element={<CommitteeDashboard />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/testing_razorpay" element={<TestingRazorpay />} />
         </Routes>
         <Footer />
       </BrowserRouter>
